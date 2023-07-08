052d1aa8f1f45f5032854d406197847dca6b5d78
vint1315 <vint1315@gmail.com>
2023-07-08 11:51:34 +0530

    Added Oscillator Script
    Object movement script for level design

---
+++ TOB/Assets/Scripts/Oscilator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+        Debug.Log(startingPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) {return;}
+
+        float cycles = Time.time / period; // constantly growing over time
+        
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalcuated to go from 0 to 1 so it cleaner
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
